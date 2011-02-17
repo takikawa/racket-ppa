2db033bdb4d8e3405eb934b1a38296f8face5ebb
David Bremner <bremner@debian.org>
2011-02-17 07:11:47 -0400

    Importing racket_5.1.orig.tar.gz

--- collects/honu/core/lang/reader.rkt
+++
@@ -1,6 +0,0 @@
-#lang s-exp syntax/module-reader
-
-honu/core/main
-
-#:read read-honu
-#:read-syntax read-honu-syntax
--- collects/meta/web/stubs/all.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket/base
-
-(provide planet)
-
-(require "planet.rkt" "blog.rkt"      ; these need to be copied to the service
-         "pre.rkt" "git.rkt"          ; / these are used on the
-         "mailman.rkt" "dirlist.rkt"  ; \ server directly
-         "wiki.rkt")
